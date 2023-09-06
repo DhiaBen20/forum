8139d4480eb5e90e49b702ad4614c4874793f73e
DhiaBen20 <100513202+DhiaBen20@users.noreply.github.com>
2023-09-06 10:17:33 +0100

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,10 +4,8 @@ namespace Database\Seeders;
 
 use App\Models\Reply;
 use App\Models\Thread;
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -20,11 +18,5 @@ class DatabaseSeeder extends Seeder
     {
         $thread = Thread::factory()->create();
         Reply::factory(5)->create(['thread_id' => $thread->id]);
-
-        User::factory()->create([
-            'email' => 'dhia.eddine97@gmail.com',
-            'name' => 'Dhia Bendjedia',
-            'password' => Hash::make('aze-123456')
-        ]);
     }
 }
